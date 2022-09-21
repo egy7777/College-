ebaf752526a8cb6e85667c31d9b1354b4912a932
egy7777 <75612807+egy7777@users.noreply.github.com>
2022-09-21 17:08:38 -0400

    Create Question1.java

---
+++ CPS-1231/4th Class/Question1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+        	
+//declration statement
+			double length,v,a; 
+	        Scanner in=new Scanner(System.in);
+	        
+//read input
+	        System.out.println("plz enter a ");
+	        System.out.println("plz enter v ");
+
+	        	a=in.nextDouble();
+	        	v=in.nextDouble();
+// I want v to be int not double
+		 	
+
+			
+			
+//length=v*v/2a
+	        length=(v*v)/(2*a);
+
+System.out.println("length= "+length);
+			 
+	}
+
+}
