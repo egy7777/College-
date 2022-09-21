75d74439a6c66d6fc0c62660c7868b4a491ea9a4
egy7777 <75612807+egy7777@users.noreply.github.com>
2022-09-21 17:08:57 -0400

    Add files via upload

---
+++ CPS-1231/4th Class/example2.java
@@ -0,0 +1,34 @@
+
+public class example2 {
+
+	public static void main(String[] args) {
+		
+		/*
+		int x=2;
+		x=x+2+x;
+		// x=2+2+2
+		// x=6
+		
+		
+		x=x+3;
+		// x=6+3=9
+		// that one i don't get   (the 6 carries over from the top)
+		
+		System.out.println(x);
+		*/
+		
+		int k=2;
+		int k--;
+		System.out.println(k);
+
+		
+
+		
+		
+		
+		
+		
+		
+	}
+
+}
