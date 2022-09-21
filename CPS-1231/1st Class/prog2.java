774e77c3049aeafbfea0f0b10ef9ec1690aeec40
egy7777 <75612807+egy7777@users.noreply.github.com>
2022-09-21 17:01:46 -0400

    Add files via upload

---
+++ CPS-1231/1st Class/prog2.java
@@ -0,0 +1,33 @@
+
+public class prog2 {
+
+	public static void main(String[] args) {
+		
+System.out.println("1+2="+1+2);
+System.out.println("=====================");
+System.out.println("======Ben Abraham========");
+System.out.println("=====================");
+System.out.println("=======Kean University========");
+System.out.println("=====================");
+
+	//end of main "function"
+
+// end of class 
+
+
+// data type nameofvariable ;          <<<<< how to write a integer 
+
+//declration statment
+int result; 
+int value;
+
+//assigment statemnet 
+// how to store value
+result=100;
+//print the value
+System.out.print(result);
+
+
+	}
+	
+}	
