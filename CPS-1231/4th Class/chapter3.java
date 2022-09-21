8a5b13816622ad6a8dcd218a18b4a87c011e0fb7
egy7777 <75612807+egy7777@users.noreply.github.com>
2022-09-21 17:09:48 -0400

    Add files via upload

---
+++ CPS-1231/4th Class/chapter3.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class chapter3 {
+
+	public static void main(String[] args) {
+
+		Scanner in=new Scanner(System.in);
+		
+		int value; 
+		
+		System.out.println("plz enter a value ");
+			value=in.nextInt();
+			
+			
+	if(value>=0) {
+		System.out.println("Positive");
+	}
+	if(value<0) {
+		System.out.println("negative");
+	}
+		
+
+		}
+
+	}
+
