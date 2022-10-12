d0f8e3374e575f12b7468bfb61a74d6888f02640
Ben <75612807+egy7777@users.noreply.github.com>
2022-10-12 14:58:41 -0400

    Update Area2.java

--- CPS-1231/Area2.java
+++ CPS-1231/Area2.java
@@ -4,29 +4,34 @@ public class Area2 {
 
 	public static void main(String[] args) {
 
-//program is sequintal, you have to define the variable and then print.
 int x;
 double y;
 
-//defined y as a double to take decimle points
-
 Scanner input=new Scanner(System.in);
 
-// the program needs input during running time
 
-System.out.println("please enter the value>>");
+System.out.println("Please enter the value>>");
 x=input.nextInt();
 y=input.nextDouble();
+ 
 
-//next double is to define as a double
-//Scanner is the class used to read any value during running time. 
-//"new" is the 
-
-System.out.println("the value of x is "+x);
-System.out.println("the value of y is "+y);
+System.out.println("The value of x is "+x);
+System.out.println("The value of y is "+y);
 
 
 
+		
+		
+		
+
+
+		
+		
+		
+		
+		
+		
+//nts:not.done
 
 	}
 
